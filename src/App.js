6fe234a35305402fd77e8844726d71a402e3ad46
Bünyamin BULUT <bunyaminbulut.ce@gmail.com>
2023-06-14 18:50:51 +0300

    add fvicon

--- src/App.js
+++ src/App.js
@@ -5,51 +5,53 @@ import { FacebookFilled, TwitterCircleFilled, InstagramFilled, LinkedinFilled } 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div class="item">
-          <img src={logo} className="loader-pulse" alt="logo" />
-        </div>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          <code>Websitemiz Yapım Aşamasındadır.</code>
-        </p>
-        <div className='social_media'>
-          <a
-            className="facebook"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FacebookFilled />
-          </a>
-          <a
-            className="twitter"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <TwitterCircleFilled />
-          </a>
-          <a
-            className="instagram"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InstagramFilled />
-          </a>
-          <a
-            className="linkedin"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <LinkedinFilled />
-          </a>
-        </div>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <div class="item">
+            <img src={logo} className="loader-pulse" alt="logo" />
+          </div>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>
+            <code>Websitemiz Yapım Aşamasındadır.</code>
+          </p>
+          <div className='social_media'>
+            <a
+              className="facebook"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FacebookFilled />
+            </a>
+            <a
+              className="twitter"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <TwitterCircleFilled />
+            </a>
+            <a
+              className="instagram"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <InstagramFilled />
+            </a>
+            <a
+              className="linkedin"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedinFilled />
+            </a>
+          </div>
+        </header>
+      </div>
+    </>
   );
 }
 
